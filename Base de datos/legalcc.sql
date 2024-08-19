348d04bb9ab9d592e79ddfc771fde28d6618573d
xXSalvadorAndradeXx <salvadorandrade7089@gmail.com>
2024-08-19 13:52:06 -0600

    Base

--- Base de datos/legalcc.sql
+++ Base de datos/legalcc.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 14-05-2024 a las 16:36:00
+-- Tiempo de generación: 19-08-2024 a las 21:51:45
 -- Versión del servidor: 10.4.32-MariaDB
--- Versión de PHP: 8.0.30
+-- Versión de PHP: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -50,9 +50,6 @@ CREATE TABLE `audiencias` (
 --
 
 INSERT INTO `audiencias` (`id`, `titulo`, `caso`, `modalidad`, `fecha`, `hora`, `imputado`, `victima`, `delito`, `descripcion`, `juzgado`, `abogado`, `fiscal`, `sala`, `juez_suplente`) VALUES
-(4, 'Plazos 2', 'Caso 1', 'Virtual', '2024-04-26', '13:00:00', 'Ramon', 'Juan Carlos Mejia', 'Violacion', 'Ramon abuzo fisicamente de a Juan Carlos de maneras repetidas', 'Juzgado 2', 'Abogado 1', 'Fiscal 1', 'Sala 1', 'Suplente 3'),
-(5, 'Primera Audiencia', 'Caso 2', 'Virtual', '2024-04-27', '13:33:00', 'Emilio', 'Salvador', 'Hurto', 'Hurto culposo', 'Juzgado 3', 'Abogado 2', 'Fiscal 3', 'Sala 2', 'Suplente 3'),
-(6, 'Cambio de Medidas', 'Caso 2', 'Virtual', '2024-04-27', '13:00:00', 'Salvador', 'Emilio', 'Homicidio', 'Cambio de medidas', 'Juzgado 2', 'Abogado 2', 'Fiscal 2', 'Sala 3', 'Suplente 3'),
 (7, 'Primera', '2024-05-12-04-04-41-5949', 'Virtual', '2024-05-16', '13:46:00', 'Maria', 'Maria', 'Hurto', '12345678', 'Juzgado 3', 'Abogado 2', 'Fiscal 3', 'Sala 1', 'Suplente 1');
 
 -- --------------------------------------------------------
@@ -68,16 +65,46 @@ CREATE TABLE `casos` (
   `imputado` varchar(255) DEFAULT NULL,
   `tipo_delito` varchar(255) DEFAULT NULL,
   `documento` varchar(255) DEFAULT NULL,
-  `fecha_creacion` timestamp NOT NULL DEFAULT current_timestamp()
+  `fecha_creacion` timestamp NOT NULL DEFAULT current_timestamp(),
+  `estado` varchar(10) DEFAULT 'activo'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `casos_archivados`
+--
+
+CREATE TABLE `casos_archivados` (
+  `referencia` varchar(255) NOT NULL,
+  `victima` varchar(255) DEFAULT NULL,
+  `imputado` varchar(255) DEFAULT NULL,
+  `tipo_delito` varchar(255) DEFAULT NULL,
+  `archivos_documento` text DEFAULT NULL,
+  `fecha_creacion` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Volcado de datos para la tabla `casos`
+-- Volcado de datos para la tabla `casos_archivados`
 --
 
-INSERT INTO `casos` (`id`, `referencia`, `victima`, `imputado`, `tipo_delito`, `documento`, `fecha_creacion`) VALUES
-(14, '2024-05-12-04-04-41-5949', 'Maria', 'Salvador', 'Asalto', NULL, '2024-05-12 02:04:41'),
-(15, '2024-05-13-04-16-17-4724', 'Salvador', 'Maria', 'Asalto', NULL, '2024-05-13 02:16:17');
+INSERT INTO `casos_archivados` (`referencia`, `victima`, `imputado`, `tipo_delito`, `archivos_documento`, `fecha_creacion`) VALUES
+('2024-08-19-21-46-47-5646', 'Salvador', 'Maria', 'robo', NULL, '2024-08-19 13:46:47'),
+('2024-08-19-21-49-48-1461', 'Salvador', 'MAria', 'robo', NULL, '2024-08-19 13:49:48');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `delete_requests`
+--
+
+CREATE TABLE `delete_requests` (
+  `id` int(11) NOT NULL,
+  `nombre_usuario` varchar(255) NOT NULL,
+  `razon` text NOT NULL,
+  `fecha_hora` datetime NOT NULL,
+  `fecha_solicitud` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -90,16 +117,30 @@ CREATE TABLE `documentos` (
   `caso_referencia` varchar(255) DEFAULT NULL,
   `nombre_archivo` varchar(255) DEFAULT NULL,
   `tipo_archivo` varchar(255) DEFAULT NULL,
-  `ubicacion_archivo` varchar(255) DEFAULT NULL
+  `ubicacion_archivo` varchar(255) DEFAULT NULL,
+  `archivado` tinyint(1) DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `documentos_archivados`
+--
+
+CREATE TABLE `documentos_archivados` (
+  `id` int(11) NOT NULL,
+  `caso_referencia` varchar(255) DEFAULT NULL,
+  `nombre_archivo` text DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Volcado de datos para la tabla `documentos`
+-- Volcado de datos para la tabla `documentos_archivados`
 --
 
-INSERT INTO `documentos` (`id`, `caso_referencia`, `nombre_archivo`, `tipo_archivo`, `ubicacion_archivo`) VALUES
-(6, '2024-05-12-04-04-41-5949', 'Ejemplo de Trabajo Final.pdf', 'application/pdf', 'documentos/Ejemplo de Trabajo Final.pdf'),
-(7, '2024-05-13-04-16-17-4724', 'Investigaciónes.pdf', 'application/pdf', 'documentos/Investigaciónes.pdf');
+INSERT INTO `documentos_archivados` (`id`, `caso_referencia`, `nombre_archivo`) VALUES
+(1, '2024-08-19-21-46-47-5646', 'storyboard-plantilla-aprendercine.pdf'),
+(2, '2024-08-19-21-46-47-5646', 'storyboard-plantilla-aprendercine.pdf'),
+(3, '2024-08-19-21-46-47-5646', 'storyboard-plantilla-aprendercine.pdf');
 
 -- --------------------------------------------------------
 
@@ -112,17 +153,22 @@ CREATE TABLE `evidencias` (
   `caso_referencia` varchar(255) DEFAULT NULL,
   `nombre_archivo` varchar(255) DEFAULT NULL,
   `tipo_archivo` varchar(255) DEFAULT NULL,
-  `ubicacion_archivo` varchar(255) DEFAULT NULL
+  `ubicacion_archivo` varchar(255) DEFAULT NULL,
+  `tipo_evidencia` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+-- --------------------------------------------------------
+
 --
--- Volcado de datos para la tabla `evidencias`
+-- Estructura de tabla para la tabla `evidencias_archivadas`
 --
 
-INSERT INTO `evidencias` (`id`, `caso_referencia`, `nombre_archivo`, `tipo_archivo`, `ubicacion_archivo`) VALUES
-(33, '2024-05-12-04-04-41-5949', 'thousand-sunny-ys.jpg', NULL, 'uploads/thousand-sunny-ys.jpg'),
-(34, '2024-05-12-04-04-41-5949', 'nfw - unbound (2).mp3', NULL, 'uploads/nfw - unbound (2).mp3'),
-(35, '2024-05-13-04-16-17-4724', 'inicio.png', NULL, 'uploads/inicio.png');
+CREATE TABLE `evidencias_archivadas` (
+  `id` int(11) NOT NULL,
+  `caso_referencia` varchar(255) DEFAULT NULL,
+  `tipo_evidencia` varchar(255) DEFAULT NULL,
+  `descripcion` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -166,6 +212,18 @@ ALTER TABLE `casos`
   ADD UNIQUE KEY `referencia` (`referencia`);
 
 --
+-- Indices de la tabla `casos_archivados`
+--
+ALTER TABLE `casos_archivados`
+  ADD PRIMARY KEY (`referencia`);
+
+--
+-- Indices de la tabla `delete_requests`
+--
+ALTER TABLE `delete_requests`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indices de la tabla `documentos`
 --
 ALTER TABLE `documentos`
@@ -173,9 +231,23 @@ ALTER TABLE `documentos`
   ADD KEY `caso_referencia` (`caso_referencia`);
 
 --
+-- Indices de la tabla `documentos_archivados`
+--
+ALTER TABLE `documentos_archivados`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `caso_referencia` (`caso_referencia`);
+
+--
 -- Indices de la tabla `evidencias`
 --
 ALTER TABLE `evidencias`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `caso_referencia` (`caso_referencia`);
+
+--
+-- Indices de la tabla `evidencias_archivadas`
+--
+ALTER TABLE `evidencias_archivadas`
   ADD PRIMARY KEY (`id`),
   ADD KEY `caso_referencia` (`caso_referencia`);
 
@@ -199,19 +271,37 @@ ALTER TABLE `audiencias`
 -- AUTO_INCREMENT de la tabla `casos`
 --
 ALTER TABLE `casos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+
+--
+-- AUTO_INCREMENT de la tabla `delete_requests`
+--
+ALTER TABLE `delete_requests`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT de la tabla `documentos`
 --
 ALTER TABLE `documentos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- AUTO_INCREMENT de la tabla `documentos_archivados`
+--
+ALTER TABLE `documentos_archivados`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT de la tabla `evidencias`
 --
 ALTER TABLE `evidencias`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=45;
+
+--
+-- AUTO_INCREMENT de la tabla `evidencias_archivadas`
+--
+ALTER TABLE `evidencias_archivadas`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de la tabla `usuarios`
@@ -230,10 +320,22 @@ ALTER TABLE `documentos`
   ADD CONSTRAINT `documentos_ibfk_1` FOREIGN KEY (`caso_referencia`) REFERENCES `casos` (`referencia`);
 
 --
+-- Filtros para la tabla `documentos_archivados`
+--
+ALTER TABLE `documentos_archivados`
+  ADD CONSTRAINT `documentos_archivados_ibfk_1` FOREIGN KEY (`caso_referencia`) REFERENCES `casos_archivados` (`referencia`);
+
+--
 -- Filtros para la tabla `evidencias`
 --
 ALTER TABLE `evidencias`
   ADD CONSTRAINT `evidencias_ibfk_1` FOREIGN KEY (`caso_referencia`) REFERENCES `casos` (`referencia`);
+
+--
+-- Filtros para la tabla `evidencias_archivadas`
+--
+ALTER TABLE `evidencias_archivadas`
+  ADD CONSTRAINT `evidencias_archivadas_ibfk_1` FOREIGN KEY (`caso_referencia`) REFERENCES `casos_archivados` (`referencia`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
